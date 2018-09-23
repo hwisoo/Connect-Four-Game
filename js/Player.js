c0a8b6838f075b475813f346ea339c40b73527fe
James Cho <hwisoocho@gmail.com>
2018-09-23 15:38:31 -0700

    createTokens() Method

--- js/Player.js
+++ js/Player.js
@@ -4,6 +4,21 @@ class Player {
         this.id = id;
         this.color = color;
         this.active = active;
-        this.tokens = [];
+        this.tokens = this.createTokens(21);
+    }
+
+    /**
+     * Creates token objects for player
+     * @param {integer} num - Number of token objects to be created 
+     */
+    
+    createTokens(num) {
+        const tokens = [];
+        for (let i = 0; i< num; i++) {
+            let toke = new Token(i, this);
+            tokens.push(token);
+        }
+
+        return tokens;
     }
 }
