f3e409b631e7a6a2f69f4759437dbf6ecbc1f75b
James Cho <hwisoocho@gmail.com>
2018-09-24 13:20:17 -0700

    moveLeft() , moveRight() methods

--- js/Token.js
+++ js/Token.js
@@ -3,6 +3,7 @@ class Token {
         this.owner = owner;
         this.id = `token-${index}-${owner.id}`;
         this.dropped = false;
+        this.columnLocation = 0;
     }
 
     /**Gets associated htmlToken.
@@ -13,6 +14,15 @@ class Token {
     }
 
     /**
+     * Gets left offset of html element.
+     * @return {string} Left offset of token object's htmlToken. 
+     */
+    get offsetLeft(){
+        return this.htmlToken.offsetLeft;
+    }
+
+
+    /**
      * Draws new HTML token.
      */
     drawHTMLToken(){
@@ -21,5 +31,25 @@ class Token {
         token.setAttribute('id', this.id);
         token.setAttribute('class', 'token');
         token.style.backgroundColor = this.owner.color;
+    }
+
+    /**
+     * Moves html token one column to left.
+     */
+    moveLeft() {
+        if (this.columnLocation > 0){
+            this.htmlToken.style.left = this.offsetLeft - 76;
+            this.columnLocation -= 1;
+        }
+    }
+
+    /**
+     * Moves html token one column to right.
+     */
+    moveRight(columns) {
+        if (this.columnLocation < columns - 1) {
+            this.htmlToken.style.left = this.offsetLeft + 76;
+            this.columnLocation += 1;
+        }
     }
 }
